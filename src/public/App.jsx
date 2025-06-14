e500acc54b129754b4ed39edc78148eb90aba449
Sushant Dhakal <sushantdhakal18@gmail.com>
2025-06-15 00:52:12 +0545

    Differentt changes made tailwind css started

---
+++ src/public/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Signup from './Register';  // Assuming Register.jsx exports Signup component
+import Homepage from './Homepage';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+}
+
+export default App;
